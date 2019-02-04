0f4bf002d609002293c94d53cebec05c494bd7d4
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-02-04 19:55:39 +0530

    added static folder && single page application

--- server/index.js
+++ server/index.js
@@ -14,6 +14,17 @@ const posts = require('./routes/api/posts');
 // Use routes
 app.use('/api/posts', posts);
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  //static folder
+  app.use(express.static(__filename + '/public'));
+
+  // handle single page application
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+}
+
 // db config
 // const db = require('./config/keys').mongoURI;
 
